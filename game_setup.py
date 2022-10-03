5f3505fb4afa119dd314f5001a4ea89e1910a80d
Richard Gregorio <thekrunchyfrog@gmail.com>
2022-10-03 11:32:57 -0400

    moved game code to own file

---
+++ game_setup.py
@@ -0,0 +1,62 @@
+import opentdb
+import random
+from html import unescape
+
+
+class GameSetup:
+    score = 0
+
+    def __init__(self):
+        self.conn = opentdb.Opentdb()
+
+    def get_categories(self):
+        return self.conn.get_catagories()
+
+    def get_questions(self):
+        questions = []
+        raw_questions = self.conn.get_questions(11)
+        i = 0
+        for raw_question in raw_questions:
+            answers = []
+            questions.append({"question": unescape(raw_question["question"])})
+            answers = [{unescape(raw_question["correct_answer"]): True}]
+            for answer in raw_question["incorrect_answers"]:
+                answers.append({unescape(answer): False})
+            questions[i]["answers"] = self.prepare_answers(answers)
+            i = i + 1
+        return questions
+
+    def prepare_answers(self, answers):
+        prepped_answers = {}
+        for x in ("a", "b", "c", "d"):
+            item1 = random.choice(answers)
+            prepped_answers[x] = item1
+            answers.remove(item1)
+        return prepped_answers
+
+    def read_answers(self, question):
+        for key in question["answers"]:
+            for key2 in question["answers"][key]:
+                print(key + ": " + key2)
+
+    def get_correct_answer(self, question):
+        for answer in ("a", "b", "c", "d"):
+            if list(question["answers"][answer].values())[0] is True:
+                return answer
+
+    def check_answer(self, question, guess):
+        is_it = list(question["answers"][guess].values())[0]
+        correct_answer = self.get_correct_answer(question)
+
+        if is_it is True:
+            self.score += 1
+            print("Correct! Score is now: " + str(self.score))
+        else:
+            print(
+                "Wrong! Correct answer was: "
+                + correct_answer
+                + ": "
+                + list(question["answers"][correct_answer].keys())[0]
+                + " Your score is still: "
+                + str(self.score)
+            )
--- robie2xl.py
+++ robie2xl.py
@@ -1,79 +1,26 @@
-import opentdb
-import random
-
-from html import unescape
+from game_setup import *
 
 
 class Robie2xl:
-    def __init__(self):
-        self.conn = opentdb.Opentdb()
-
     def main():
         pass
-
-    def list_categories(self):
-        categories = self.conn.get_catagories()
-
-        for category in categories:
-            print(str(category["id"]) + " " + category["name"])
-
-    def prepare_questions(self):
-        questions = []
-        raw_questions = self.conn.get_questions(11)
-        i = 0
-        for raw_question in raw_questions:
-            answers = []
-            questions.append({"question": unescape(raw_question["question"])})
-            answers = [{unescape(raw_question["correct_answer"]): True}]
-            for answer in raw_question["incorrect_answers"]:
-                answers.append({unescape(answer): False})
-            questions[i]["answers"] = self.prepare_answers(answers)
-            i = i + 1
-        return questions
-
-    def prepare_answers(self, answers):
-        prepped_answers = {}
-        for x in ("a", "b", "c", "d"):
-            item1 = random.choice(answers)
-            prepped_answers[x] = item1
-            answers.remove(item1)
-        return prepped_answers
-
-    def read_answers(answers):
-        for key in question["answers"]:
-            for key2 in question["answers"][key]:
-                print(key + ": " + key2)
 
     # if __name__ == "__main__":
     #    main()
 
 
-questions = Robie2xl().prepare_questions()
+game = GameSetup()
+questions = game.get_questions()
 
 score = 0
 
 for question in questions:
     print(question["question"])
-    Robie2xl.read_answers(question["answers"])
+    game.read_answers(question)
 
     guess = input("What is your guess: ")
-    is_it = list(question["answers"][guess].values())[0]
 
-    for answer in ("a", "b", "c", "d"):
-        if list(question["answers"][answer].values())[0] is True:
-            correct_answer = answer
+    game.check_answer(question, guess)
 
-    if is_it is True:
-        score += 1
-        print("Correct! Score is now: " + str(score))
-    else:
-        print(
-            "Wrong! Correct answer was: "
-            + correct_answer
-            + ": "
-            + list(question["answers"][correct_answer].keys())[0]
-            + " Your score is still: "
-            + str(score)
-        )
 
-print("Your final score is: " + str(score) + " out of 10.")
+print("Your final score is: " + str(game.score) + " out of 10.")
